12054b965e62ddaf8a3f83264a79768ac60c4a2f
Yair-Martinez <yairmartinezespitia@gmail.com>
2023-09-01 16:05:09 -0500

    feat(backend): instalar las dependencias necesarias para la migración, además de migrar el modelo Tarea

---
+++ TaskManager.Backend/TaskManager.Backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Backend.Models.Entities;
+
+namespace TaskManager.Backend.Data
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<Tarea> Tareas { get; set; }
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+	}
+}
---
+++ TaskManager.Backend/TaskManager.Backend/Migrations/20230901204832_TaskManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskManager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}

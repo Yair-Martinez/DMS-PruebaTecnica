6dbc55b384ee778dd26de042d12e1d6760c1365a
Yair-Martinez <yairmartinezespitia@gmail.com>
2023-09-02 16:05:47 -0500

    fix(backend): incluir campo Id a la clase TareaOnlyReadDto

--- TaskManager.Backend/TaskManager.Backend/Models/DTOs/Tarea/TareaReadOnlyDto.cs
+++ TaskManager.Backend/TaskManager.Backend/Models/DTOs/Tarea/TareaReadOnlyDto.cs
@@ -4,6 +4,7 @@ namespace TaskManager.Backend.Models.DTOs.Tarea
 {
 	public class TareaReadOnlyDto
 	{
+		public Guid Id { get; set; }
 		public string Titulo { get; set; } = string.Empty;
 		public string Descripcion { get; set; } = string.Empty;
 		public Estados Estado { get; set; }

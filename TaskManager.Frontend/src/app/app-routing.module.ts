d363e1c32f136912f6ccf17efc65cf5b5999b690
Yair-Martinez <yairmartinezespitia@gmail.com>
2023-09-02 15:32:31 -0500

    feat(frontend): crear modulo task-manager e incluir en el routing de la app

--- TaskManager.Frontend/src/app/app-routing.module.ts
+++ TaskManager.Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "task-manager",
+    loadChildren: () => import('./modules/task-manager/task-manager.module').then(m => m.TaskManagerModule)
+  },
+  {
+    path: "",
+    redirectTo: "task-manager",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
